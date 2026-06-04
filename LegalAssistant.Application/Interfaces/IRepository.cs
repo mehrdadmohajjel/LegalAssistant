649a149ba035e61a50e3495e3e4a581dd8aa277d
Golshan <mehrdad.mohajjel@gmail.com>
2026-06-04 11:58:54 +0330

    Generic Interface Added

---
+++ LegalAssistant.Application/Interfaces/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegalAssistant.Application.Interfaces
+{
+    public interface IRepository<T>
+       where T : class
+    {
+        Task<T?> GetByIdAsync(
+            object id,
+            CancellationToken cancellationToken);
+
+        Task<List<T>> GetAllAsync(
+            CancellationToken cancellationToken);
+
+        Task AddAsync(
+            T entity,
+            CancellationToken cancellationToken);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ LegalAssistant.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegalAssistant.Application.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken);
+    }
+}
